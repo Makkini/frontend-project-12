edcdf5ee638b0778d853f116c1f0de0402148034
Makkinni <finmakmislgood@gmail.com>
2025-03-24 16:37:30 +0300

    add rollbar.js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,37 +7,43 @@ import NotFoundPage from "./pages/NotFoundPage/NotFoundPage.jsx";
 import Navbar from "./components/Navbar.jsx";
 import SignupPage from "./pages/SignupPage/SignupPage.jsx";
 import PrivateRoute from "./app/PrivateRoute.jsx";
+import { Provider, ErrorBoundary } from '@rollbar/react';
+import {rollbar} from "./services/rollbar.js";
 
 const App = () => {
   return (
-    <div className='h-100'>
-      <Navbar />
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route
-          path="/"
-          element={
-            <PrivateRoute>
-              <ChatPage />
-            </PrivateRoute>
-          }
-        />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
-      <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="light"
-      />
-    </div>
+    <Provider instance={rollbar}>
+      <ErrorBoundary>
+        <div className='h-100'>
+          <Navbar />
+          <Routes>
+            <Route path="/login" element={<LoginPage />} />
+            <Route
+              path="/"
+              element={
+                <PrivateRoute>
+                  <ChatPage />
+                </PrivateRoute>
+              }
+            />
+            <Route path="/signup" element={<SignupPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+          <ToastContainer
+            position="top-right"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="light"
+          />
+        </div>
+      </ErrorBoundary>
+    </Provider>
   );
 };
 
---
+++ frontend/src/services/rollbar.js
@@ -0,0 +1,18 @@
+import Rollbar from "rollbar";
+
+const rollbarConfig = {
+  accessToken: import.meta.env.VITE_ROLLBAR_ACCESS_TOKEN,
+  environment: import.meta.env.VITE_ENV || 'development',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+  payload: {
+    client: {
+      javascript: {
+        code_version: '1.0.0',
+        source_map_enabled: true,
+      }
+    }
+  }
+};
+
+export const rollbar = new Rollbar(rollbarConfig);
